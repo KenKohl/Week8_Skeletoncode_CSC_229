e6daea059988bce5b770b1e3ed4f933b7eb4f139
KenKohl <Kohlka@farmingdale.edu>
2024-03-23 11:31:17 -0400

    Bubble sort code completed. Time complexity still needed

--- src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
@@ -12,12 +12,36 @@ public class BubbleSort {
 
     public static void bubbleSort(int a[], int size) {
         int outer, inner, temp;
+        boolean hasSwapped;
         for (outer = size - 1; outer > 0; outer--) { // counting down
+            hasSwapped = false;
             for (inner = 0; inner < outer; inner++) { // bubbling up
+                if(a[inner] > a[inner + 1]){
+                    temp = a[inner];
+                    a[inner] = a[inner + 1];
+                    a[inner + 1] = temp;
+                    hasSwapped = true;
+                }
                 //ToDo 3: complete this algorithm, test it, provide its time complexity
+            }
+            if(hasSwapped == false){
+                break;
             }
         }
     }
-    
+
+    public static void print(int array[], int size){
+        for(int i = 0; i < size; i++){
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void main(String args[]){
+        int array[] = {18, 36, 9, 27, 81, 54, 45, 63, 72, 90};
+        int size = array.length;
+        bubbleSort(array,size);
+        System.out.println("The sorted array is: ");
+        print(array,size);
+    }
     
 }
