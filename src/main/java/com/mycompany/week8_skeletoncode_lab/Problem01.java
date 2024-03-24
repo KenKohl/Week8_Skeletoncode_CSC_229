0cb225a2900ac122f82578ad4f3172bf51da9e64
KenKohl <Kohlka@farmingdale.edu>
2024-03-23 20:55:32 -0400

    All code written for problems 1-4. Time complexity and comments still needed.

--- src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.week8_skeletoncode_lab;
-
+import java.util.Scanner;
+import java.lang.Math;
 /**
  *
  * @author MoaathAlrajab and Ken Kohlhof
@@ -11,12 +12,34 @@ package com.mycompany.week8_skeletoncode_lab;
 public class Problem01 {
     
     public static long getSumOfPrimes(int n){
+        int sum = 0;
+        for(int i = 2; i <= n; i++){
+            for(int j = 2; j <= i; j++){
+                if(j==i){
+                    sum+=i;
+                    break;
+                }
+                else if(i % j==0) {
+                    break;
+                }
+            }
+        }
     // Todo 04: Develop a method that returns the sum of the prime numbers between 1 and n
     //          Test your solution
-    //          Analyze its space and time  
+    //          Analyze its space and time
+        return sum;
+    }
 
-        //delete code below. Only there to remove error
-        return -1;
+    public static void main(String args[]){
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Please enter an upper bound.");
+
+        int upperBound= scnr.nextInt();
+
+        Long sum = getSumOfPrimes(upperBound);
+
+        System.out.println(sum);
     }
     
 }
